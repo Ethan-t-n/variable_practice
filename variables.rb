6f39d6f34d901b26471a2136cd3a278be2feb588
Ethan Nguyen <etnguyen16@gmail.com>
2022-04-13 20:12:20 -0600

    add all commits

---
+++ variables.rb
@@ -0,0 +1,20 @@
+# these are the stings I will use
+# the strings have variables that are name, age, and favorite_drink
+name = "ethan"
+location = "colorado"
+favorite_drink = "Fanta"
+# these are the Integers I will use
+# these integers consists of age and wieght which both have no decimals
+age = 22
+weight = 145
+# these are the floats I will use. font_size and money_on_hand which have decimals
+font_size = 2.5
+money_on_hand = 20.50
+# these are the booleans I will use. they are likes_Fanta, hates_music, and owns_pet which all have a set true or false on them
+likes_Fanta = true
+hates_music = false
+owns_pet = true
+# reassigning strings here which are just name, age, and favorite_drink
+name = "john"
+location = "california"
+favorite_drink = "pepsi"
